aab6c5883458c45ce9f4f6da60489bf17170d791
maryam <marychch19@gmail.com>
2024-12-27 16:45:13 +0330

    new

--- script.js
+++
@@ -1,6 +0,0 @@
-let toggler = document.getElementById("switch");
-toggler.addEventListener("click", () => {
-  toggler.checked === true
-    ? (document.body.style.background = "black")
-    : (document.body.style.background = "white");
-});
